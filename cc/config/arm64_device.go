ad4703697928077f47641a26a5f60746693afa55
Kampalus <kampalus@protonmail.ch>
2025-09-18 23:07:42 +0200

    rework CPU variant handling
    Use -mtune instead of -mcpu because clang does false assumptions resulting in illegal opcodes.
    
    This happened to me when using -mcpu=cortex-a55 on Kryo 3xx which are based off of Cortex-A55 but lack dotprod, as dotprod is optional for A55.
    
    Signed-off-by: Kampalus <kampalus@protonmail.ch>

--- cc/config/arm64_device.go
+++ cc/config/arm64_device.go
@@ -57,23 +57,28 @@ var (
 
 	arm64CpuVariantCflags = map[string][]string{
 		"cortex-a53": []string{
-			"-mcpu=cortex-a53",
+			"-mtune=cortex-a53",
 		},
 		"cortex-a55": []string{
-			"-mcpu=cortex-a55",
+			"-mtune=cortex-a55",
+		},
+		"cortex-a72": []string{
+			"-mtune=cortex-a72",
+		},
+		"cortex-a73": []string{
+			"-mtune=cortex-a73",
 		},
 		"cortex-a75": []string{
-			// Use the cortex-a55 since it is similar to the little
-			// core (cortex-a55) and is sensitive to ordering.
-			"-mcpu=cortex-a55",
+			"-mtune=cortex-a75",
 		},
 		"cortex-a76": []string{
-			// Use the cortex-a55 since it is similar to the little
-			// core (cortex-a55) and is sensitive to ordering.
-			"-mcpu=cortex-a55",
+			"-mtune=cortex-a76",
 		},
 		"kryo": []string{
-			"-mcpu=kryo",
+			"-mtune=kryo",
+		},
+		"oryon": []string{
+			"-mtune=oryon-1",
 		},
 	}
 )
@@ -103,7 +108,12 @@ func init() {
 
 	pctx.StaticVariable("Arm64CortexA53Cflags", strings.Join(arm64CpuVariantCflags["cortex-a53"], " "))
 	pctx.StaticVariable("Arm64CortexA55Cflags", strings.Join(arm64CpuVariantCflags["cortex-a55"], " "))
+	pctx.StaticVariable("Arm64CortexA72Cflags", strings.Join(arm64CpuVariantCflags["cortex-a72"], " "))
+	pctx.StaticVariable("Arm64CortexA73Cflags", strings.Join(arm64CpuVariantCflags["cortex-a73"], " "))
+	pctx.StaticVariable("Arm64CortexA75Cflags", strings.Join(arm64CpuVariantCflags["cortex-a75"], " "))
+	pctx.StaticVariable("Arm64CortexA76Cflags", strings.Join(arm64CpuVariantCflags["cortex-a76"], " "))
 	pctx.StaticVariable("Arm64KryoCflags", strings.Join(arm64CpuVariantCflags["kryo"], " "))
+	pctx.StaticVariable("Arm64OryonCflags", strings.Join(arm64CpuVariantCflags["oryon"], " "))
 
 	pctx.StaticVariable("Arm64FixCortexA53Ldflags", "-Wl,--fix-cortex-a53-843419")
 }
@@ -112,11 +122,12 @@ var (
 	arm64CpuVariantCflagsVar = map[string]string{
 		"cortex-a53": "${config.Arm64CortexA53Cflags}",
 		"cortex-a55": "${config.Arm64CortexA55Cflags}",
-		"cortex-a72": "${config.Arm64CortexA53Cflags}",
-		"cortex-a73": "${config.Arm64CortexA53Cflags}",
-		"cortex-a75": "${config.Arm64CortexA55Cflags}",
-		"cortex-a76": "${config.Arm64CortexA55Cflags}",
+		"cortex-a72": "${config.Arm64CortexA72Cflags}",
+		"cortex-a73": "${config.Arm64CortexA73Cflags}",
+		"cortex-a75": "${config.Arm64CortexA75Cflags}",
+		"cortex-a76": "${config.Arm64CortexA76Cflags}",
 		"kryo":       "${config.Arm64KryoCflags}",
+		"oryon":      "${config.Arm64OryonCflags}",
 	}
 
 	arm64CpuVariantLdflags = map[string]string{
--- cc/config/arm_device.go
+++ cc/config/arm_device.go
@@ -78,7 +78,7 @@ var (
 
 	armCpuVariantCflags = map[string][]string{
 		"cortex-a7": []string{
-			"-mcpu=cortex-a7",
+			"-mtune=cortex-a7",
 			"-mfpu=neon-vfpv4",
 			// Fake an ARM compiler flag as these processors support LPAE which clang
 			// don't advertise.
@@ -87,10 +87,25 @@ var (
 			"-D__ARM_FEATURE_LPAE=1",
 		},
 		"cortex-a8": []string{
-			"-mcpu=cortex-a8",
+			"-mtune=cortex-a8",
+			"-mfpu=neon-vfpv4",
+			// Fake an ARM compiler flag as these processors support LPAE which clang
+			// don't advertise.
+			// TODO This is a hack and we need to add it for each processor that supports LPAE until some
+			// better solution comes around. See Bug 27340895
+			"-D__ARM_FEATURE_LPAE=1",
+		},
+		"cortex-a9": []string{
+			"-mtune=cortex-a9",
+			"-mfpu=neon-vfpv4",
+			// Fake an ARM compiler flag as these processors support LPAE which clang
+			// don't advertise.
+			// TODO This is a hack and we need to add it for each processor that supports LPAE until some
+			// better solution comes around. See Bug 27340895
+			"-D__ARM_FEATURE_LPAE=1",
 		},
 		"cortex-a15": []string{
-			"-mcpu=cortex-a15",
+			"-mtune=cortex-a15",
 			"-mfpu=neon-vfpv4",
 			// Fake an ARM compiler flag as these processors support LPAE which clang
 			// don't advertise.
@@ -99,7 +114,7 @@ var (
 			"-D__ARM_FEATURE_LPAE=1",
 		},
 		"cortex-a32": []string{
-			"-mcpu=cortex-a32",
+			"-mtune=cortex-a32",
 			"-mfpu=neon-vfpv4",
 			// Fake an ARM compiler flag as these processors support LPAE which clang
 			// don't advertise.
@@ -108,7 +123,7 @@ var (
 			"-D__ARM_FEATURE_LPAE=1",
 		},
 		"cortex-a53": []string{
-			"-mcpu=cortex-a53",
+			"-mtune=cortex-a53",
 			"-mfpu=neon-fp-armv8",
 			// Fake an ARM compiler flag as these processors support LPAE which clang
 			// don't advertise.
@@ -117,7 +132,25 @@ var (
 			"-D__ARM_FEATURE_LPAE=1",
 		},
 		"cortex-a55": []string{
-			"-mcpu=cortex-a55",
+			"-mtune=cortex-a55",
+			"-mfpu=neon-fp-armv8",
+			// Fake an ARM compiler flag as these processors support LPAE which clang
+			// don't advertise.
+			// TODO This is a hack and we need to add it for each processor that supports LPAE until some
+			// better solution comes around. See Bug 27340895
+			"-D__ARM_FEATURE_LPAE=1",
+		},
+		"cortex-a72": []string{
+			"-mtune=cortex-a72",
+			"-mfpu=neon-fp-armv8",
+			// Fake an ARM compiler flag as these processors support LPAE which clang
+			// don't advertise.
+			// TODO This is a hack and we need to add it for each processor that supports LPAE until some
+			// better solution comes around. See Bug 27340895
+			"-D__ARM_FEATURE_LPAE=1",
+		},
+		"cortex-a73": []string{
+			"-mtune=cortex-a73",
 			"-mfpu=neon-fp-armv8",
 			// Fake an ARM compiler flag as these processors support LPAE which clang
 			// don't advertise.
@@ -126,7 +159,7 @@ var (
 			"-D__ARM_FEATURE_LPAE=1",
 		},
 		"cortex-a75": []string{
-			"-mcpu=cortex-a55",
+			"-mtune=cortex-a75",
 			"-mfpu=neon-fp-armv8",
 			// Fake an ARM compiler flag as these processors support LPAE which clang
 			// don't advertise.
@@ -135,7 +168,7 @@ var (
 			"-D__ARM_FEATURE_LPAE=1",
 		},
 		"cortex-a76": []string{
-			"-mcpu=cortex-a55",
+			"-mtune=cortex-a76",
 			"-mfpu=neon-fp-armv8",
 			// Fake an ARM compiler flag as these processors support LPAE which clang
 			// don't advertise.
@@ -144,7 +177,7 @@ var (
 			"-D__ARM_FEATURE_LPAE=1",
 		},
 		"krait": []string{
-			"-mcpu=krait",
+			"-mtune=krait",
 			"-mfpu=neon-vfpv4",
 			// Fake an ARM compiler flag as these processors support LPAE which clang
 			// don't advertise.
@@ -153,9 +186,7 @@ var (
 			"-D__ARM_FEATURE_LPAE=1",
 		},
 		"kryo": []string{
-			// Use cortex-a53 because the GNU assembler doesn't recognize -mcpu=kryo
-			// even though clang does.
-			"-mcpu=cortex-a53",
+			"-mtune=kryo",
 			"-mfpu=neon-fp-armv8",
 			// Fake an ARM compiler flag as these processors support LPAE which clang
 			// don't advertise.
@@ -197,10 +228,15 @@ func init() {
 	pctx.StaticVariable("ArmGenericCflags", strings.Join(armCpuVariantCflags[""], " "))
 	pctx.StaticVariable("ArmCortexA7Cflags", strings.Join(armCpuVariantCflags["cortex-a7"], " "))
 	pctx.StaticVariable("ArmCortexA8Cflags", strings.Join(armCpuVariantCflags["cortex-a8"], " "))
+	pctx.StaticVariable("ArmCortexA9Cflags", strings.Join(armCpuVariantCflags["cortex-a9"], " "))
 	pctx.StaticVariable("ArmCortexA15Cflags", strings.Join(armCpuVariantCflags["cortex-a15"], " "))
 	pctx.StaticVariable("ArmCortexA32Cflags", strings.Join(armCpuVariantCflags["cortex-a32"], " "))
 	pctx.StaticVariable("ArmCortexA53Cflags", strings.Join(armCpuVariantCflags["cortex-a53"], " "))
 	pctx.StaticVariable("ArmCortexA55Cflags", strings.Join(armCpuVariantCflags["cortex-a55"], " "))
+	pctx.StaticVariable("ArmCortexA72Cflags", strings.Join(armCpuVariantCflags["cortex-a72"], " "))
+	pctx.StaticVariable("ArmCortexA73Cflags", strings.Join(armCpuVariantCflags["cortex-a73"], " "))
+	pctx.StaticVariable("ArmCortexA75Cflags", strings.Join(armCpuVariantCflags["cortex-a75"], " "))
+	pctx.StaticVariable("ArmCortexA76Cflags", strings.Join(armCpuVariantCflags["cortex-a76"], " "))
 	pctx.StaticVariable("ArmKraitCflags", strings.Join(armCpuVariantCflags["krait"], " "))
 	pctx.StaticVariable("ArmKryoCflags", strings.Join(armCpuVariantCflags["kryo"], " "))
 }
@@ -217,15 +253,15 @@ var (
 		"":               "${config.ArmGenericCflags}",
 		"cortex-a7":      "${config.ArmCortexA7Cflags}",
 		"cortex-a8":      "${config.ArmCortexA8Cflags}",
-		"cortex-a9":      "${config.ArmGenericCflags}",
+		"cortex-a9":      "${config.ArmCortexA9Cflags}",
 		"cortex-a15":     "${config.ArmCortexA15Cflags}",
 		"cortex-a32":     "${config.ArmCortexA32Cflags}",
 		"cortex-a53":     "${config.ArmCortexA53Cflags}",
 		"cortex-a55":     "${config.ArmCortexA55Cflags}",
-		"cortex-a72":     "${config.ArmCortexA53Cflags}",
-		"cortex-a73":     "${config.ArmCortexA53Cflags}",
-		"cortex-a75":     "${config.ArmCortexA55Cflags}",
-		"cortex-a76":     "${config.ArmCortexA55Cflags}",
+		"cortex-a72":     "${config.ArmCortexA72Cflags}",
+		"cortex-a73":     "${config.ArmCortexA73Cflags}",
+		"cortex-a75":     "${config.ArmCortexA75Cflags}",
+		"cortex-a76":     "${config.ArmCortexA76Cflags}",
 		"krait":          "${config.ArmKraitCflags}",
 		"kryo":           "${config.ArmKryoCflags}",
 	}
